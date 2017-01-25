5dcc81236011efe254ba1d00a60c0663c93e3dda
AlexanderNeumann <alexander.t.n@web.de>
2017-01-25 23:15:26 +0100

    Random speed
    Timer is missing.

--- LoopingThar/LoopingThar.ino
+++ LoopingThar/LoopingThar.ino
@@ -1,15 +1,27 @@
 int player[] = {A2,A3,A4,A5};
+int speedRegulationPin = 3;
+
+int intervalls[] = {0,63,127,191,255};
 int sensitivity = 900; 
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Start ");
+  // Initialize player colision pins
   for (int playerPin = 0; playerPin < (sizeof(player)/sizeof(int)); playerPin++) {
     pinMode(player[playerPin], OUTPUT);
   }
+  // Initialize speed regulation pin
+  pinMode(speedRegulationPin, OUTPUT);
 }
 
 void loop() {
+  checkCollision();
+  // ToDo Make use of a timer (delay not possible -> we need the collision detection)
+  randomSpeed();
+}
+
+void checkCollision(){
   for (int playerPin = 0; playerPin < (sizeof(player)/sizeof(int)); playerPin++) {
     if(analogRead(player[playerPin]) > sensitivity) {
       Serial.print("Player ");
@@ -18,3 +30,10 @@ void loop() {
     } 
   }
 }
+
+void randomSpeed(){
+  int ri = random(5);
+  analogWrite(3, intervalls[ri]);
+}
+
+
