b74067bba28d94e71707156c9e04b3cd0e914b35
AlexanderNeumann <alexander.t.n@web.de>
2017-01-25 04:33:29 +0100

    Refactoring ♻️

--- LoopingThar/LoopingThar.ino
+++ LoopingThar/LoopingThar.ino
@@ -1,30 +1,20 @@
-int player1 = A2;
-int player2 = A3;
-int player3 = A4;
-int player4 = A5;
+int player[] = {A2,A3,A4,A5};
 int sensitivity = 900; 
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Start ");
-  pinMode(player1, OUTPUT);
-  pinMode(player2, OUTPUT);
-  pinMode(player3, OUTPUT);
-  pinMode(player4, OUTPUT);
+  for (int playerPin = 0; playerPin < (sizeof(player)/sizeof(int)); playerPin++) {
+    pinMode(player[playerPin], OUTPUT);
+  }
 }
 
 void loop() {
-  if(analogRead(player1) > sensitivity) {
-    Serial.println("Player 1 collided");
+  for (int playerPin = 0; playerPin < (sizeof(player)/sizeof(int)); playerPin++) {
+    if(analogRead(player[playerPin]) > sensitivity) {
+      Serial.print("Player ");
+      Serial.print(playerPin);
+      Serial.println(" collided");
+    } 
   }
-  if(analogRead(player2) > sensitivity) {
-    Serial.println("Player 2 collided");
-  }
-  if(analogRead(player3) > sensitivity) {
-    Serial.println("Player 3 collided");
-  }
-  if(analogRead(player4) > 900) {
-    Serial.println("Player 4 collided");
-  }
-
 }
