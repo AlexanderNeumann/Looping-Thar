744801fa69a341a5f80ad12d1a1dd6d82eb3a815
Hinrikus Wolf <Hinrikus Wolf>
2017-02-24 12:23:30 +0100

    add pump control software

---
+++ pumpcontrol/pumpcontrol.ino
@@ -0,0 +1,72 @@
+// kate: syntax c++
+
+ // global Variable defintions
+ 
+const uint8_t pump_pin   = 12;
+
+int delay_pump = 500;
+int delay_valve = 5000;
+
+
+const uint8_t valve_pin[4] {
+  11,
+  10,
+  9,
+  8,
+};
+
+
+void give_shot(int valve) {
+    start_pump(); 
+    delay(delay_pump);
+    open_valve(valve);
+    delay(delay_valve);
+    close_valve(valve);
+    delay(delay_pump);
+    stop_pump();
+}
+
+void start_pump() {
+    digitalWrite(pump_pin,HIGH);
+    Serial.println("Start Pump");
+}
+
+void stop_pump() {
+    digitalWrite(pump_pin,LOW);
+    Serial.println("Stop Pump");
+}
+
+void open_valve(int valve) {
+    digitalWrite(valve_pin[valve],HIGH);
+    Serial.println("Open Valve");
+}
+
+void close_valve(int valve) {
+    digitalWrite(valve_pin[valve],LOW);
+    Serial.println("Close Valve");
+    
+}
+ 
+void setup() {
+  // put your setup code here, to run once:
+  
+Serial.begin(115200);
+  
+pinMode(pump_pin, OUTPUT);
+for(int i=0; i<4; i++) {
+pinMode(valve_pin[i], OUTPUT);
+}
+
+
+
+
+
+}
+
+void loop() {
+
+give_shot(0);
+
+delay(3000);
+
+}
